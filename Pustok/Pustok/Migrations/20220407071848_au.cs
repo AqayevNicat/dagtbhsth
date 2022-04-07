9df6fa09ee260bf9a20a62c167109a063d90505e
Nijat M. Aghayev <nijatma@code.local>
2022-04-07 00:45:05 -0700

    sdvdv

---
+++ Pustok/Pustok/Migrations/20220407071848_au.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pustok.Migrations
+{
+    public partial class au : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleteAuthors",
+                table: "Authors",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleteAuthor",
+                table: "Authors",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
---
+++ Pustok/Pustok/Migrations/20220407073638_svasdf.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pustok.Migrations
+{
+    public partial class svasdf : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeletedGenres",
+                table: "Genres",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeletedGenres",
+                table: "Genres");
+        }
+    }
+}
